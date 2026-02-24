3c6d4aee540ddc47324e337e0767488eab936cf6
artus-prog <artusalves15@gmail.com>
2026-02-23 21:37:41 -0300

    interface
    inicio do aprendizado da interface

--- interface.py
+++ interface.py
@@ -1,1 +1,11 @@
+import funçoes
+import main
 from tkinter import *
+
+janela_main = Tk()
+janela_main.title("Lista de compras")
+texto_main = Label(janela_main, text=("Selecione a opção desejada:"))
+texto_main.grid(column=(0), row=(0))
+botao1 = Button(janela_main, text=("Adicionar item"), command= funçoes.adicionar_item(main.lista_compras))
+botao1.grid(column=(0),row=(1))
+janela_main.mainloop()
